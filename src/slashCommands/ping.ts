dd577b9ff8ed9c074c32106e97abc6f6b1825259
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-08 04:21:13 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -1,26 +1,28 @@
-import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js"
+import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js";
 import { SlashCommand } from "../types";
 import webhookClient from "../index";
+
 const testCommand: SlashCommand = {
     command: new SlashCommandBuilder()
         .setName("test")
         .setDescription("Test command")
-        .addStringOption(option => {
-            
-            return option
+        .addStringOption(option =>
+            option
                 .setName("content")
                 .setDescription("this is a parameter for a command")
-                .setRequired(false);
-        }),
+                .setRequired(false)
+        ),
     execute: async (interaction) => {
         const options: { [key: string]: string | number | boolean } = {};
         for (let i = 0; i < interaction.options.data.length; i++) {
             const element = interaction.options.data[i];
             if (element.name && element.value) options[element.name] = element.value;
         }
-        console.log('interaction: '+ interaction)
-        const reply = interaction.reply({
-            
+
+        console.log('interaction:', interaction);
+
+        const message = await interaction.reply({
+            fetchReply: true,
             embeds: [
                 new EmbedBuilder()
                     .setAuthor({ name: "Response Title" })
@@ -28,10 +30,14 @@ const testCommand: SlashCommand = {
                     Your ping: ${interaction.client.ws.ping}
                     Your input: ${options.content}`)
             ]
-        })
-        console.log("Sent message ID:", reply.id);
+        });
+        console.log("Message :", message);
+        console.log("Message ID:", message.id);
+        console.log("Message content:", message.content);
+        console.log("Message channel:", message.channel.id);
+        console.log("Message URL:", message.url);
     },
     cooldown: 3
-}
+};
 
 export default testCommand;
