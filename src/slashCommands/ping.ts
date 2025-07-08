ba0544150062da6a82e961e714752321dcab0927
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2025-07-09 01:51:33 +0800

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -28,7 +28,7 @@ const testCommand: SlashCommand = {
       url
     });
 
-    await interaction.deferReply({ ephemeral: true });
+    //await interaction.deferReply({ ephemeral: true });
 
     try {
       const res = await fetch(url);
@@ -74,14 +74,14 @@ const testCommand: SlashCommand = {
       } else {
         console.warn("⚠️ Webhook sent but no message object was returned.");
       }
-      /*
+      
       // Safely edit reply — no message link if not available
       const replyText = webhookMessage?.url
         ? `✅ Content sent via webhook.\n[Jump to Message](${webhookMessage.url})`
         : `✅ Webhook request sent to: ${url}`;
 
       await interaction.editReply({ content: replyText });
-      */
+      
     } catch (error: any) {
       console.error("❌ Fetch or send error:", error);
       await interaction.editReply({
