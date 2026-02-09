56e7b06016c1b421c3ab6369f601a39577435451
Glenn Marten <59699274+ToastBoxMe@users.noreply.github.com>
2026-02-09 11:20:34 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+// 👇 central role guard
+export const PERMISSIONS = {
+  PROJECT_CREATE: [
+    "1321122630744412241",// Neotrix
+    "1468897007530672202" //developer noetrix
+  ],
+};
